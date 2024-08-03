91b804e00aa879c87c790a2c611260855d19f0ed
Saurish Nagrath <saurishnagrath2005@GMAIL.COM>
2024-08-03 13:02:44 +0530

    website (functional)
    Made the whole fucking website

---
+++ Clearist/index.js
@@ -0,0 +1,67 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+} 
+
+
+
+const list_items=document.querySelectorAll("myUL");
+const count= list_items.length;
+console.log(count);
+
+
+const task_complete=document.getElementsByClassName("checked")
+const val_task_complete=task_complete.length;
+const bar_fill=(val_task_complete/list_items)*100;
+const per_val_task_completed=bar_fill
+
+const widthBar=document.getElementsByClassName("filled").style.width;
+
+
