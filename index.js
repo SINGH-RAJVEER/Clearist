685fb92a9e24582e823ec6c4a4e35cd22650dd53
Rajveer Singh <therajveersingh@icloud.com>
2024-08-05 21:11:44 +0530

    nothing new

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
+for (var i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
